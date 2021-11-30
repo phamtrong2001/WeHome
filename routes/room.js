2cff49907cae16c29300bc8328a2458e05ce86d1
Trọng Phạm <phamtrong2k1@gmail.com>
2021-11-30 20:50:11 +0700

    commit

--- routes/room.js
+++ routes/room.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const {models, db} = require('../sequelize/conn');
+const {Sequelize, Op, QueryTypes} = require("sequelize");
 
 async function search(req, res) {
     try {
-        const {lat, long} = req.body;
+        const lat = req.body.latitude;
+        const long = req.body.longitude;
+        const radius = req.body.radius;
+        const distance = db.Sequelize.literal("6371 * acos(cos(radians("+lat+")) * cos(radians(latitude)) * cos(radians("+long+") - radians(longitude)) + sin(radians("+lat+")) * sin(radians(latitude)))");
         const rooms = await models.room.findAll({
             attributes: [
                 'room_id',
@@ -13,10 +17,16 @@ async function search(req, res) {
                 'longitude',
                 'host_id',
                 'price',
-                [db.Sequelize.literal("6371 * acos(cos(radians("+lat+")) * cos(radians(latitude)) * cos(radians("+long+") - radians(longitude)) + sin(radians("+lat+")) * sin(radians(latitude)))"),'distance']
+                [distance,'distance']
             ],
+            where: db.Sequelize.where(distance, "<=", radius),
+            // having: {
+            //     distance: {
+            //         [Op.lte]: radius
+            //     }
+            // },
             order: db.Sequelize.col('distance'),
-            limit: 10
+            limit: 100
         });
         res.status(200).send(rooms);
     } catch (err) {
