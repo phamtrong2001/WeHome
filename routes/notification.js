fb10d9ddd34dbd7c2bec6abf206e73aa437ddd08
npqhp <91194263+npqhp@users.noreply.github.com>
2021-12-03 14:21:54 +0700

    Add files via upload

---
+++ routes/notification.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const models = require("../sequelize/conn");
+const router = express.Router();
+
+/**
+ * Get notification by user id
+ * @author: admin, host, user
+ */
+router.get('/',async function (rep,res){
+    const payload = jwt.decode(req.headers.authorization.split(' ')[1]);
+    await models.rental.findAll({
+        where: {
+            user_id: payload.user_id
+        }
+    }).then(function (project){
+        if (project.length) res.status(200).json(project);
+        else res.status(404).json({'message': 'Notification not found'});
+    })
+})
+
+module.exports = router;
