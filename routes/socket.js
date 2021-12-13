a175635a10faa0a5d0a056f07a21f83e7a743504
Trọng Phạm <phamtrong2k1@gmail.com>
2021-12-13 10:26:06 +0700

    fix socket

--- routes/socket.js
+++ routes/socket.js
@@ -5,32 +5,37 @@ const socketapi = {
 };
 
 io.on("connection", function(socket) {
-    const userId = socket.handshake.query.userId;
-    socket.join(userId);
-    console.log(`User ${userId} connected`);
-    socket.on("send_rental", async (receiver_id, content) => {
-        io.to(receiver_id.toString()).emit("receive_rental", content, new Date());
-        const newNotification = {
-            user_id: receiver_id,
-            content: content,
-            type: "RENTAL",
-            status: "UNREAD"
-        };
-        await models.notification.create(newNotification);
-        console.log(content);
-    });
+    try {
+        const userId = socket.handshake.query.userId;
+        socket.join(userId);
+        console.log(`User ${userId} connected`);
+        socket.on("send_rental", async (receiver_id, content) => {
+            io.to(receiver_id.toString()).emit("receive_rental", content, new Date());
+            const newNotification = {
+                user_id: receiver_id,
+                content: content,
+                type: "RENTAL",
+                status: "UNREAD"
+            };
+            await models.notification.create(newNotification);
+            console.log(content);
+        });
 
-    socket.on("send_feedback", async (roomId, content) => {
-        const room = await models.room.findByPk(roomId);
-        io.to(room.host_id.toString()).emit("receive_feedback", roomId, content, new Date());
-        const newNotification = {
-            user_id: room.host_id,
-            content: content,
-            type: "FEEDBACK",
-            status: "UNREAD"
-        };
-        await models.notification.create(newNotification);
-    })
+        socket.on("send_feedback", async (roomId, content) => {
+            const room = await models.room.findByPk(roomId);
+            io.to(room.host_id.toString()).emit("receive_feedback", roomId, content, new Date());
+            const newNotification = {
+                user_id: room.host_id,
+                content: content,
+                type: "FEEDBACK",
+                status: "UNREAD"
+            };
+            await models.notification.create(newNotification);
+        })
+    } catch (err) {
+        console.log(err);
+        socket.emit('error', err.message);
+    }
 });
 
 module.exports = socketapi;
