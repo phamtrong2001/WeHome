501ec746e79bf2229ec0f2255aa4358a20d80b88
Trọng Phạm <phamtrong2k1@gmail.com>
2021-12-14 21:03:42 +0700

    commit

--- utils/email.js
+++ utils/email.js
@@ -17,8 +17,8 @@ module.exports.sendEmail = function sendEmail(email, password) {
             text: "Let's login with this password and change your password",
             html: "<h3>Chúng tôi đã nhận được yêu cầu reset mật khẩu của bạn.</h3><br>" +
                 "<p>Hãy đăng nhập bằng mật khẩu dưới đây và đổi lại mật khẩu của bạn tại " +
-                "<a href='/home'>WeHome</a>" + " :" +
-                "<h3 style='align-content: center'><strong>"+ password + "</strong></h3>"
+                "<a href='/home'>WeHome</a>" + " : </p>" +
+                "<h3>" + password + "</h3>"
         }
 
         transporter.sendMail(mailOptions, function (err, info) {
