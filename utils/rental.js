3509de0fe86832b6816ce9b259bc0adee6f02112
Trọng Phạm <phamtrong2k1@gmail.com>
2021-12-21 22:39:43 +0700

    add noti for rejected rental

--- utils/rental.js
+++ utils/rental.js
@@ -3,6 +3,31 @@ const {Op} = require("sequelize");
 
 module.exports.deleteRentalUnconfirmed = async function deleteRentalUnconfirmed(room_id, begin_date, end_date) {
     try {
+        let rentals = await models.rental.findAll({
+            where: {
+                room_id: room_id,
+                status: "UNCONFIRMED",
+                [Op.or]: [
+                    {
+                        begin_date: {
+                            [Op.between]: [begin_date, end_date]
+                        }
+                    },
+                    {
+                        end_date: {
+                            [Op.between]: [begin_date, end_date]
+                        }
+                    }
+                ]
+            }
+        });
+
+        for (let rental of rentals) {
+            let client_id = rental.client_id;
+            //TODO: send notification to client
+
+        }
+
         await models.rental.destroy({
             where: {
                 room_id: room_id,
