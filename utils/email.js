d4daae843d2acb8b911c717a63f4635c75ef4af3
Quoc <19020021@vnu.edu.vn>
2022-06-24 10:36:42 +0700

    fix sendEmail for mobile

--- utils/email.js
+++ utils/email.js
@@ -17,8 +17,8 @@ module.exports.sendEmail = function sendEmail(email, password) {
             subject: 'Reset password WeHome',
             text: "Let's login with this password and change your password",
             html: "<h3>Chúng tôi đã nhận được yêu cầu reset mật khẩu của bạn.</h3><br>" +
-                "<p>Hãy đăng nhập bằng mật khẩu dưới đây và đổi lại mật khẩu của bạn tại " +
-                "<a href='https://www.wehomeapp.tk/'>WeHome</a>" + " : </p>" +
+                "<p>Hãy đăng nhập bằng mật khẩu dưới đây và đổi lại mật khẩu của bạn tại We Home" +
+                // "<a href='https://www.wehomeapp.tk/'>WeHome</a>" + " : </p>" +
                 "<h3>" + password + "</h3>"
         }
 
